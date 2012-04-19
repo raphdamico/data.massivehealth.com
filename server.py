27de03de1b378d56d27b6d85d0164029e1571379
Aza Raskin <aza@massivehealth.com>
2012-04-18 21:58:47 -0700

    A simple server to run the site. Type "python server.py"

---
+++ server.py
@@ -0,0 +1,12 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "Serving files at http://localhost:%d" % PORT
+
+httpd.serve_forever()
